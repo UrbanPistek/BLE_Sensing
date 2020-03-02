64a00817c10e46e8a3d9445b1698f7c3ed51afb0
UrbanPistek <urban.pistek@gmail.com>
2020-03-01 20:56:35 -0500

    Data input functions complete

--- BLE_RSSI_CNN_1D_v1.py
+++ BLE_RSSI_CNN_1D_v1.py
@@ -24,36 +24,64 @@ else:
     device = torch.device("cpu")
     print("Running on the CPU")
 
-LABELS = {'Empty': 0,'One person': 1, 'Two or more people':2} #DNN needs numeric labels
+EMPTY = 0
+ONE_PERSON = 1
+TWO_PLUS_PEOPLE = 2
+LABELS = [EMPTY, ONE_PERSON, TWO_PLUS_PEOPLE] #DNN needs numeric labels
 TIME_STEPS = 50
-STEP_INCREMET = 1
+TRAIN_LEN = 60 #Train Dataset length (number of time matrices)
+TEST_LEN = 12 #Test Dataset length (number of time matrices)
 
 def load_dataset():
     data = pd.read_csv('dataset1.csv')
+    # Normalize the Data - Note Data is all negative so dividing by istelf twice will make it positive
+    data['B1'] = data['B1'] / data['B1'].min()
+    data['B2'] = data['B2'] / data['B2'].min()
+    data['B3'] = data['B3'] / data['B3'].min()
+    data['B4'] = data['B4'] / data['B4'].min()
 
-def make_traing_data(df, time_steps, step):
+    data['Label'].replace({"Empty": 0, "One person": 1, "Two people": 2}, inplace=True) #Replace Strings with values
+
+    # Round numbers
+    data = data.round({'B1': 6, 'B2': 6, 'B3': 6, 'B4': 6})
+
+    return data
+
+def make_traing_data(df, time_steps, train_len):
     N_FEATURES = 4
 
     segments = []
     labels = []
-    for i in range(0, len(df) - time_steps, step): #Verify
-        B1 = df['B1'].values[i: i + time_steps]
-        B2 = df['B2'].values[i: i + time_steps]
-        B3 = df['B3'].values[i: i + time_steps]
-        B4 = df['B4'].values[i: i + time_steps]
-        # Retrieve the most often used label in this segment
-        '''label = stats.mode(df[label_name][i: i + time_steps])[0][0] #modify label_name'''
-        segments.append([B1, B2, B3, B4])
-        labels.append(label)
+    for idx in tqdm(range(train_len)):
+        for i in range(time_steps*idx, time_steps*(idx+1)): #Verify
+            B1 = df['B1'].values[i: i + time_steps]
+            B2 = df['B2'].values[i: i + time_steps]
+            B3 = df['B3'].values[i: i + time_steps]
+            B4 = df['B4'].values[i: i + time_steps]
+            # Retrieve the most often used label in this segment
+            label = stats.mode(df['Label'][i: i + time_steps])[0][0] #modify label_name
+            segments.append([B1, B2, B3, B4])
+            labels.append(label)
 
-        # Bring the segments into a better shape
-        reshaped_segments = np.asarray(segments, dtype=np.float32).reshape(-1, time_steps, N_FEATURES) #Modify
-        labels = np.asarray(labels) #Modify
+            # Bring the segments into a better shape
+            reshaped_segments = np.asarray(segments, dtype=np.float64).reshape(-1, time_steps, N_FEATURES) #Modify
+            value = np.asarray(labels) #Modify
 
-    return reshaped_segments, labels
+    return reshaped_segments, value
 
 #Pandas working 
-data = pd.read_csv('dataset1.csv')
-columns = data.columns
+data = load_dataset()
 print(data.head(5))
-print(len(data)) #3666
+#print(len(data)) #3666
+
+train_X, train_y = make_traing_data(data, TIME_STEPS, 3)
+print(train_X[0:3])
+print(train_y[0:3])
+print(len(train_X))
+print(len(train_y))
+
+print("********** Training Data Shape *******************")
+print('x_train shape: ', train_X.shape)
+print(train_X.shape[0], 'training samples')
+print('y_train shape: ', train_y.shape)
+
